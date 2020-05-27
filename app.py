7312f43f10f63d823da187be019ce364c72a68d6
tayutaedomo <tayutaedomo@gmail.com>
2020-05-27 13:17:44 +0900

    Bugfix of logging

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    app.logger.info('handle_message: ' + event)
+    app.logger.info('handle_message: ' + event.message.text)
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=event.message.text))
